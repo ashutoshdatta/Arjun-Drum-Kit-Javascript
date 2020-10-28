d9908a9ea668307646041612e8c54b0e834eed81
ashutoshdatta <72577102+ashutoshdatta@users.noreply.github.com>
2020-10-28 13:52:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+guest = prompt("Please enter your name: ");
+alert("Hi " + guest + " Welcome to Arjun's Drum site");
+
+//Detecting button press
+numDrumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numDrumButtons; i++) {
+  //  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+    console.log(this.innerHTML);
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  })
+}
+
+//Detecting keyboard press
+document.addEventListener("keydown", function () {
+  console.log(event);
+  makeSound(event.key);
+  buttonAnimation(event.key);
+})
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "l":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    default: console.log(key);
+  }
+  return;
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  // animate button for key press
+  activeButton.classList.add("pressed");
+  // undo animation after 100ms to simulate key unpress appearances
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
+/*
+function handleClick() {
+  alert("Got clicked");
+}
+*/
